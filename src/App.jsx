a06695e143f5c4723c3f4be7cb2ffa4ae0c59ec2
npapoutsakis <nikospapoutsak@gmail.com>
2023-11-26 21:41:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// Importing important components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Note from "./components/Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
