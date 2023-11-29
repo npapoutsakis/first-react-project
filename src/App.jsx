a394b5326fdec888ce87690ecf1f26af08b48f32
npapoutsakis <nikospapoutsak@gmail.com>
2023-11-29 13:40:29 +0200

    added dynamically notes

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,15 @@ import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
+import notes from "./notes.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))}
       <Footer />
     </div>
   );
